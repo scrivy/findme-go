781a966842aec9f9de85d3986837e5d43a425cbb
Daniel <daniel@memebox.com>
2016-04-10 23:00:41 -0700

    getTiles works

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"io"
 	"log"
 	"net/http"
-	//	"regexp"
+	"os"
+	"regexp"
+
+	"golang.org/x/net/websocket"
 )
 
-var addr = flag.String("addr", ":5000", "http service address")
+var xyzRegex *regexp.Regexp
 
 func main() {
-	flag.Parse()
-	go h.run()
+	xyzRegex = regexp.MustCompile(`^\/tiles\/(\d+)\/(\d+)\/(\d+)\.png$`)
+
 	http.HandleFunc("/tiles/", getTiles)
-	http.HandleFunc("/ws", serveWs)
+	http.Handle("/ws", websocket.Handler(wsHandler))
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	err := http.ListenAndServe(*addr, nil)
+
+	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func wsHandler(ws *websocket.Conn) {
+	io.Copy(ws, ws)
 }
 
 func getTiles(w http.ResponseWriter, r *http.Request) {
@@ -28,5 +35,35 @@ func getTiles(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(r.URL.Path)
+	if !xyzRegex.MatchString(r.URL.Path) {
+		http.Error(w, "wrong format", 400)
+		return
+	}
+	xyz := xyzRegex.FindStringSubmatch(r.URL.Path)
+	xyzPath := "tiles/" + xyz[1] + "/" + xyz[2] + "/"
+	xyzFile := xyzPath + xyz[3] + ".png"
+
+	if _, err := os.Stat(xyzFile); os.IsNotExist(err) {
+		resp, err := http.Get("http://a.tile.thunderforest.com/transport/" + xyz[1] + "/" + xyz[2] + "/" + xyz[3] + ".png")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+		err = os.MkdirAll(xyzPath, 0755)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		out, err := os.Create(xyzFile)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		io.Copy(out, resp.Body)
+		out.Close()
+		log.Println("downloaded " + xyzFile)
+	}
+
+	http.ServeFile(w, r, xyzFile)
 }
